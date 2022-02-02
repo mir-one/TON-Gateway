fd432752cc234f6bb9b09b8d39836ceaaad9e0c2
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2022-02-02 23:34:01 +0300

    Фильтр-хук зарегистрации PHP-класса в качестве платёжного шлюза WooCommerce

--- ton-gateway.php
+++ ton-gateway.php
@@ -7,3 +7,12 @@
  * Author URI: https://t.me/inozemtsev_roman
  * Version: 0.0.1
  *
+/*
+ * Фильтр-хук зарегистрации PHP-класса в качестве платёжного шлюза WooCommerce
+ */
+add_filter( 'woocommerce_payment_gateways', 'ton_register_gateway_class' );
+ 
+function ton_register_gateway_class( $gateways ) {
+	$gateways[] = 'WC_TON_Gateway'; // название класса TON, добавляем в общий массив
+	return $gateways;
+}
