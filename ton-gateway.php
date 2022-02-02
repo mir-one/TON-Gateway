f27d048eaae64fab5c0555fdb839ff9f0419ff7d
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2022-02-02 23:31:43 +0300

    Create ton-gateway.php

---
+++ ton-gateway.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Plugin Name: Платёжный шлюз TON
+ * Plugin URI: https://github.com/mir-one/ton-gateway
+ * Description: Принимайте оплату с помощью TON у себя на сайте!
+ * Author: Roman Inozemtsev
+ * Author URI: https://t.me/inozemtsev_roman
+ * Version: 0.0.1
+ *
